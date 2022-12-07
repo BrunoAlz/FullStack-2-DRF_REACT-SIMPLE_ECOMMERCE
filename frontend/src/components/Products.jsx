cb0fdd704295675b8e04fcf8efa2bb0a5d970ce1
Bruno Alves <bruno6821@gmail.com>
2022-12-07 05:48:05 -0300

    Criando o componente Ratings

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,22 +1,27 @@
 import { Card, Button } from "react-bootstrap";
 
+// COMPONENTS
+import Ratings from "./Ratings";
+
 const Products = ({ product }) => {
   return (
     <Card className="my-3" style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={product.image} />
+      <a href={`/product/${product._id}`}>
+        <Card.Img variant="top" src={product.image} />
+      </a>
       <Card.Body>
         <Card.Title>{product.name}</Card.Title>
         <Card.Text>
-          <i className="fas fa-star" style={{ color: "gold" }}></i>
-          {product.rating} de {product.numReviews} Avaliações
+          <Ratings
+            value={product.rating}
+            text={`${product.numReviews} Avaliações`}
+            color={"#F6BE00"}
+          />
         </Card.Text>
         <Card.Text>
-          <h4>Preço: R$ {product.price}</h4>
+          <h4>R$ {product.price}</h4>
           Estoque: {product.countInStock}
         </Card.Text>
-        <a href={`/product/${product._id}`}>
-          <Button variant="primary">Detalhes</Button>
-        </a>
       </Card.Body>
     </Card>
   );

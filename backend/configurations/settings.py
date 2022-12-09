04cc44b1ce9a9594af9376574f8bd36e80927770
Bruno Alves <bruno6821@gmail.com>
2022-12-09 15:39:01 -0300

    Ajustes nos arquivos Staticos

--- backend/configurations/settings.py
+++ backend/configurations/settings.py
@@ -133,12 +133,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    BASE_DIR / 'frontend/build/static'
-]
-
-
 MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
